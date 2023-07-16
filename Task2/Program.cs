0ef3097b9b00f26bdf9cca9e7a7c1f343ab5b56d
NikoRino <95122320+NikoRino@users.noreply.github.com>
2023-07-16 15:32:35 +0700

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,7 +1,7 @@
 void Distance(int ax, int ay, int az, int bx, int by, int bz)
 {
     double distans = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
-    Console.WriteLine($"Расстояние межэду двух точек: {distans}!");
+    Console.WriteLine($"Расстояние между двух точек: {distans}!");
 }
 
 int GetInput(string text)
